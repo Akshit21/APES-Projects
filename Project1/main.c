50d2617db71487c2820f129e98c0731632286591
Akshit Shah <aksh9247@colorado.edu>
2018-02-22 13:50:11 -0800

    Initial Framework

---
+++ Project1/main.c
@@ -0,0 +1,64 @@
+/**************************************************************************************************
+* Copyright (C) 2017 by Akshit Shah, Shuting Guo
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Akshit Shah, Shuting Guo, Prof. Alex Fosdick and the University of Colorado are 
+* not liable for any misuse of this material.
+***************************************************************************************************/
+/***************************************************************************************************
+* @author : Akshit Shah
+* @date : 02/22/2018
+*
+* @file : main.c
+* @brief : Entry Point of the Program. Thread creation and initiailization
+*
+* @tool : Compiler - GCC, Linker - GDB, Cross Compiler - arm-linux-gnueabihf-gcc
+* @hardware : Beagle Bone Green AM335x Arm Corex - A8, TMP106, APDS-9301
+* @reference : 
+***************************************************************************************************/
+
+#include "main.h"
+
+int main()
+{
+    int32_t isThreadCreated = 0;
+
+    /* Create Logging Thread */
+    isThreadCreated = pthread_create(&threads[LOG_THREAD],NULL,&logThread,NULL);
+    if(isThreadCreated != 0)
+    {
+	printf("Thread Creation failed, error code - %d\n", isThreadCreated);
+    }
+
+    /* Create Temperature Thread */
+    isThreadCreated = pthread_create(&threads[TEMP_THREAD],NULL,&tempThread,NULL);
+    if(isThreadCreated != 0)
+    {
+	printf("Thread Creation failed, error code - %d\n", isThreadCreated);
+    }
+
+    /* Create Light Thread */
+    isThreadCreated = pthread_create(&threads[LIGHT_THREAD],NULL,&lightThread,NULL);
+    if(isThreadCreated != 0)
+    {
+	printf("Thread Creation failed, error code - %d\n", isThreadCreated);
+    }
+
+    /* Create Socket Thread */
+    isThreadCreated = pthread_create(&threads[SOCKET_THREAD],NULL,&socketThread,NULL);
+    if(isThreadCreated != 0)
+    {
+	printf("Thread Creation failed, error code - %d\n", isThreadCreated);
+    }
+
+    /* Main Task Handling Code will come here */
+
+    /* join the pthread with the existing processes */
+    pthread_join(threads[LOG_THREAD], NULL);
+    pthread_join(threads[TEMP_THREAD], NULL);
+    pthread_join(threads[LIGHT_THREAD], NULL);
+    pthread_join(threads[SOCKET_THREAD], NULL);   
+
+}
---
+++ Project1/main.h
@@ -0,0 +1,49 @@
+/***************************************************************************************************
+* Copyright (C) 2017 by Akshit Shah, Shuting Guo
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Akshit Shah, Shuting Guo, Prof Alex Fosdick and the University of Colorado are 
+* not liable for any misuse of this material.
+***************************************************************************************************/
+/***************************************************************************************************
+* @author : Akshit Shah
+* @date : 02/22/2018
+*
+* @file : main.h
+* @brief : This header file provides an abstraction of thread definations and initialization 
+           variables.
+***************************************************************************************************/
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include<pthread.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<mqueue.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/signal.h>
+#include<sys/time.h>
+#include<stdint.h>
+#include<sys/types.h>
+
+/********************************************
+* D E F I N E S
+*********************************************/
+#define NUM_THREADS   (4)
+#define LOG_THREAD    (0)
+#define TEMP_THREAD   (1)
+#define LIGHT_THREAD  (2)
+#define SOCKET_THREAD (3)
+
+/********************************************
+* V A R I A B L E S
+*********************************************/
+/* Array to store thread ids */
+pthread_t threads[NUM_THREADS];
+
+#endif  /* MAIN_H_ */
