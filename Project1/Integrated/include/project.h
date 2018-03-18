fbd8f1c4f74520a814deee5fcf6b2a1cc41cc527
Akshit Shah <aksh9247@colorado.edu>
2018-03-17 18:23:36 -0700

    Comment Headers

--- Project1/Integrated/include/project.h
+++ Project1/Integrated/include/project.h
@@ -1,3 +1,19 @@
+/***************************************************************************************************
+* Copyright (C) 2017 by Akshit Shah, Shuting Guo
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is permitted as long as the files maintain this copyright. Users are
+* permitted to modify this and use it to learn about the field of embedded
+* software. Akshit Shah, Shuting Guo, Prof Alex Fosdick and the University of Colorado are
+* not liable for any misuse of this material.
+***************************************************************************************************/
+/***************************************************************************************************
+* @author : Akshit Shah, Shuting Guo
+* @date : 02/22/2018
+*
+* @file : project.h
+* @brief : This header file provides a common header interface for entire project
+***************************************************************************************************/
 #ifndef _PROJECT_H__
 #define _PROJECT_H__
 
